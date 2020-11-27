e9c91de767a46dbbd31f7598bdd40bdf06205abe
sulemansarwar10 <sulemansarwar10@gmail.com>
2020-11-28 00:25:26 +0500

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world bu suleman
+          Hello world by suleman
         </p>
         <a
           className="App-link"
